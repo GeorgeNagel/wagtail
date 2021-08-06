2b3856c18e8679d49199cc2504d477605fdd1bf0
George Nagel <gsnagel@gmail.com>
2021-08-06 16:38:22 -0400

    Negative indexing not allowed

--- wagtail/admin/views/page_privacy.py
+++ wagtail/admin/views/page_privacy.py
@@ -16,7 +16,7 @@ def set_privacy(request, page_id):
     restrictions = page.get_view_restrictions().order_by('page__depth')
     if restrictions:
         # it is possible that restrictions exist on the ancestor, this page, or both
-        restriction = restrictions[-1]
+        restriction = restrictions.last()
         ancestor_with_restriction = None
         if restrictions[0] == page:
             ancestor_with_restriction = None
