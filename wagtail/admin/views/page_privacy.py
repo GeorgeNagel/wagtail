8adfd15eb895921e71395e96f55372225f25a193
George Nagel <gsnagel@gmail.com>
2021-08-06 16:37:24 -0400

    Attempt at t improving page privacy logic

--- wagtail/admin/views/page_privacy.py
+++ wagtail/admin/views/page_privacy.py
@@ -15,8 +15,15 @@ def set_privacy(request, page_id):
     # fetch restriction records in depth order so that ancestors appear first
     restrictions = page.get_view_restrictions().order_by('page__depth')
     if restrictions:
-        restriction = restrictions[0]
-        restriction_exists_on_ancestor = (restriction.page != page)
+        # it is possible that restrictions exist on the ancestor, this page, or both
+        restriction = restrictions[-1]
+        ancestor_with_restriction = None
+        if restrictions[0] == page:
+            ancestor_with_restriction = None
+            restriction_exists_on_ancestor = False
+        else:
+            ancestor_with_restriction = restrictions[0]
+            restriction_exists_on_ancestor = True
     else:
         restriction = None
         restriction_exists_on_ancestor = False
@@ -53,12 +60,11 @@ def set_privacy(request, page_id):
             })
 
     if restriction_exists_on_ancestor:
-        # display a message indicating that there is a restriction at ancestor level -
-        # do not provide the form for setting up new restrictions
+        # display a message indicating that there is a restriction at ancestor level
         return render_modal_workflow(
             request, 'wagtailadmin/page_privacy/ancestor_privacy.html', None,
             {
-                'page_with_restriction': restriction.page,
+                'page_with_restriction': ancestor_with_restriction.page,
                 'page': page,
                 'form': form,
             }, json_data={'step': 'set_privacy'}
